66689191753b7420af2364ecfd6a2c66c54af83f
Yuvlb <yuval.bardayan@post.runi.ac.il>
2024-04-06 14:50:49 +0300

    Final version of code after shellcheck

--- q3.sh
+++ q3.sh
@@ -1,7 +1,33 @@
+#ver3 - 'working!'
 #!/bin/bash
+
+check_word() {
+	local regex include exclude unknown word="$1" colors="$2"
+
+	for i in {0..4}; do
+		char=${word:i:1}
+		char_color=${colors:i:1}
+		case ${char_color,} in
+			g) pattern+="[${char^}${char,}]";;
+			y)
+				include+="[${char^}${char,}].*"
+				pattern+="[^${char^}${char,}]"
+				;;
+			*)
+				pattern+="."
+				exclude+="${char^}${char,}"
+				;;
+		esac
+	done
+
+	#[[ $include ]] && include="[$include]"
+	[[ $exclude ]] && exclude="[$exclude]"
+	awk '!/'"${exclude:-#}"'/ && /'"$include"'/ && /^'"$pattern"'$/' ./words.txt
+}
 
 # Ask the user to enter a five-letter word
 read -p "Enter a five-letter word: " input_word
+
 # Validate the entered word length
 if [ ${#input_word} -ne 5 ]; then
     echo "Error: Word must be exactly 5 letters long."
@@ -11,13 +37,16 @@ fi
 # Ask the user to enter the colors
 read -p "Enter the colors ('s' for gray, 'y' for yellow, 'g' for green): " input_colors
 
+# Validate the colors string
+if ! [[ $input_colors =~ ^[sygSYG]{5}$ ]]; then
+    echo "Error: The colors string must contain exactly 5 characters: 's', 'y', or 'g'."
+    exit 1
+fi
+
 # Convert the input word and colors to lowercase
 input_word=$(echo "$input_word" | tr '[:upper:]' '[:lower:]')
 input_colors=$(echo "$input_colors" | tr '[:upper:]' '[:lower:]')
 
-check_word() {
-    local regex include exclude unknown word="$1" colors="$2"
-    # Function body will be implemented later
-}
-
+# Call the check_word function with the input word and colors
+check_word "$input_word" "$input_colors"
 
